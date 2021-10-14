14b608e8688808d2945ee2bb28e9dc9bff3839e3
JafarAli360 <ahmad.19058@mhs.unesa.ac.id>
2021-10-14 20:39:27 +0700

    UTS Pemrograman Framework

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <style type="text/css">* {cursor: url(http://cur.cursors-4u.net/cursors/cur-9/cur808.ani), url(http://cur.cursors-4u.net/cursors/cur-9/cur808.png), auto !important;}</style><a href="http://www.cursors-4u.com/cursor/2011/03/29/the-alien-diagonal-resize.html" target="_blank" title="The Alien - Diagonal Resize"><img src="http://cur.cursors-4u.net/cursor.png" border="0" alt="The Alien - Diagonal Resize" style="position:absolute; top: 0px; right: 0px;" /></a>
+      
+      <script src="https://cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/efek-salju.js" type="text/javascript"></script>
+        <title>Profile</title>
+        <link rel="stylesheet" type="text/css" href="{{asset ('/css/Style.css')}}">
+    </head>
+<body>
+<br><p align ="center"><b><font size="10" color="white">Curriculum Vitae</font></b></p>
+<div class="resume">
+    <div class="resume_left">
+      <div class="resume_profile">
+        <img src="{{asset ('/img/2.jpg')}}" alt="profile_pic">
+      </div>
+      <div class="resume_content">
+        <div class="resume_item resume_info">
+          <div class="title">
+            <p class="bold">Ahmad Ja'far Ali</p>
+            <p class="regular"></p>
+          </div>
+
+          <ul>
+            <li>
+              <div class="icon">
+                <i class="fas fa-map-signs"></i>
+              </div>
+              <div class="data">
+                19051397058
+              </div>
+            </li>
+            <li>
+              <div class="icon">
+                <i class="fas fa-mobile-alt"></i>
+              </div>
+              <div class="data">
+                D4 Manajemen Informatika 2019 B
+              </div>
+            </li>
+            <li>
+              <div class="icon">
+                <i class="fas fa-envelope"></i>
+              </div>
+              <div class="data">
+                16 Mei 2001, Lamongan
+              </div>
+            </li>
+            <li>
+              <div class="icon">
+                <i class="fab fa-weebly"></i>
+              </div>
+              <div class="data">
+                ................
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div class="resume_item resume_skills">
+          <div class="title">
+            <p class="bold">Kemampuan</p>
+          </div>
+          <ul>
+            <li>.
+              <div class="skill_name">
+                Web design
+              </div>
+              <div class="skill_progress">
+                <span style="width: 50%;"></span>
+              </div>
+              <div class="skill_per">50%</div>
+            </li>
+            <li>
+              <div class="skill_name">
+                Graphic desain
+              </div>
+              <div class="skill_progress">
+                <span style="width: 5%;"></span>
+              </div>
+              <div class="skill_per">5%</div>
+            </li>
+            <li>
+              <div class="skill_name">
+                line Art
+              </div>
+              <div class="skill_progress">
+                <span style="width: 25%;"></span>
+              </div>
+              <div class="skill_per">25%</div>
+            </li>
+            <li>
+              <div class="skill_name">
+                Drawing
+              </div>
+              <div class="skill_progress">
+                <span style="width: 45%;"></span>
+              </div>
+              <div class="skill_per">45%</div>
+            </li>
+            <li>
+              <div class="skill_name">
+                Editing Video
+              </div>
+              <div class="skill_progress">
+                <span style="width: 10%;"></span>
+              </div>
+              <div class="skill_per">10%</div>
+            </li>
+          </ul>
+        </div>
+        <div class="resume_item resume_social">
+          <div class="title">
+            <p class="bold">Akun</p>
+          </div>
+          <ul>
+            <li>
+              <div class="icon">
+                <i class="fab fa-facebook-square"></i>
+              </div>
+              <div class="data">
+                <p class="semi-bold">Facebook</p>
+                <p>Ahmad Ja'far Ali</p>
+              </div>
+            </li>
+            <li>
+              <div class="icon">
+                <i class="fab fa-instagram-square"></i>
+              </div>
+              <div class="data">
+                <p class="semi-bold">Instagram</p>
+                <p>ja_far360</p>
+              </div>
+            </li>
+            <li>
+              <div class="icon">
+                <i class="fab fa-pubg-mobile"></i>
+              </div>
+              <div class="data">
+                <p class="semi-bold">PUBG Mobile</p>
+                <p>NotFound360</p>
+              </div>
+            </li>
+            <li>
+              <div class="icon">
+                <i class="fab fa-mobile-legends"></i>
+              </div>
+              <div class="data">
+                <p class="semi-bold">Mobile Legends</p>
+                <p>JiSketch_Art</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+   </div>
+   <div class="resume_right">
+     <div class="resume_item resume_about">
+         <div class="title">
+            <p class="bold">Tentang Saya</p>
+          </div>
+         <p>Biodata tentang diri saya</p>
+     </div>
+     <div class="resume_item resume_work">
+         <div class="title">
+            <p class="bold">Motivasi</p>
+          </div>
+         <ul>
+             <li>
+                 <div class="date">Motivasi Sukses</div> 
+                 <div class="info">
+                      <p class="semi-bold">(Benjamin Franklin)</p> 
+                      <p>“Anda mungkin bisa menunda, tapi waktu tidak akan menunggu.”</p>
+                 </div>
+             </li>
+             <li>
+               <div class="date">Motivasi Diri Sendiri</div>
+               <div class="info">
+                      <p class="semi-bold">(Elbert Hubbard)</p> 
+                      <p>"Lakukan pekerjaanmu dengan sepenuh hati. Maka kau akan sukses."</p>
+                 </div>
+             </li>
+             <li>
+               <div class="date">Motivasi Islami</div>
+               <div class="info">
+                      <p class="semi-bold">(Imam Ghazali)</p> 
+                      <p>"Hidup lah sebagaimana semaumu, tetapi ingat, bahwa engkau akan mati. Dan cintai lah siapa yang engkau sukai, namun ingat, engkau akan berpisah dengannya. Dan berbuat lah seperti yang engkau kehendaki, namun ingat, engkau pasti akan menerima balasannya nanti."</p>
+                 </div>
+             </li>
+         </ul>
+     </div>
+     <div class="resume_item resume_education">
+       <div class="title">
+            <p class="bold">Pendidikan</p>
+          </div>
+       <ul>
+             <li>
+                 <div class="date">2019 - Sekarang</div> 
+                 <div class="info">
+                      <p class="semi-bold">Universitas Negeri Surabaya</p> 
+                      <p>Notes : "Belum punya pengalaman karena Covid-19"</p>
+                 </div>
+             </li>
+             <li>
+               <div class="date">2016 - 2019</div>
+               <div class="info">
+                      <p class="semi-bold">MAN 1 LAMONGAN</p> 
+                      <p>Notes : "Tidak pernah masuk ruang BK."</p>
+                 </div>
+             </li>
+             <li>
+                <div class="date">2013 - 2016</div>
+                <div class="info">
+                       <p class="semi-bold">MTs Darul Hikam</p> 
+                       <p>Notes : "Menjadi wakil Ketua 1 OSIS, Tetapi jarang kumpul".</p>
+                  </div>
+              </li>
+         </ul>
+     </div>
+     <div class="resume_item resume_hobby">
+       <div class="title">
+            <p class="bold">Hobi</p>
+          </div>
+        <ul>
+          <li><i class="fas fa-book">Game</i></li>
+          <li><i class="fas fa-gamepad">Ngopi</i></li>
+          <li><i class="fas fa-music">listening Music</i></li>
+          <li><i class="fab fa-pagelines">Sleep</i></li>
+       </ul>
+     </div>
+   </div>
+ </div>
+</body>
+</html>
